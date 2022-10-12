56bb84ecc50b8149085c525cb0564f957798de0e
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-12 12:47:38 +0200

    add correction

--- jsack3/main.js
+++ jsack3/main.js
@@ -1,16 +1,17 @@
 
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-
-/* for (let i = 0; i < 10; i++) {
+let sum = 0
+for (let i = 0; i < 10; i++) {
       let number = Number(prompt('inserisci un numero'))
       console.log(number);
 
+      sum+ = number
 
-} */
+}
 
 // oppure
 
-const numb1 = Number(prompt('inserisci un numero'));
+/* const numb1 = Number(prompt('inserisci un numero'));
 const numb2 = Number(prompt('inserisci un numero'));
 const numb3 = Number(prompt('inserisci un numero'));
 const numb4 = Number(prompt('inserisci un numero'));
@@ -23,4 +24,4 @@ const numb10 = Number(prompt('inserisci un numero'));
 
 //Il programma stampa la somma di tutti i numeri inseriti.
 
-console.log(Number(numb1) + Number(numb2) + Number(numb3) + Number(numb4) + Number(numb5) + Number(numb6) + Number(numb7) + Number(numb8) + Number(numb9) + Number(numb10))
+console.log(Number(numb1) + Number(numb2) + Number(numb3) + Number(numb4) + Number(numb5) + Number(numb6) + Number(numb7) + Number(numb8) + Number(numb9) + Number(numb10)) */
