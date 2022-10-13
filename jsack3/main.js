1f0e172fbb0fa348e25ddb70d7802004249e6912
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-13 12:04:11 +0200

    add while version

--- jsack3/main.js
+++ jsack3/main.js
@@ -1,16 +1,8 @@
 
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-
-/* for (let i = 0; i < 10; i++) {
-      let number = Number(prompt('inserisci un numero'))
-      console.log(number);
-
-
-} */
-
 // oppure
 
-const numb1 = Number(prompt('inserisci un numero'));
+/* const numb1 = Number(prompt('inserisci un numero'));
 const numb2 = Number(prompt('inserisci un numero'));
 const numb3 = Number(prompt('inserisci un numero'));
 const numb4 = Number(prompt('inserisci un numero'));
@@ -23,4 +15,25 @@ const numb10 = Number(prompt('inserisci un numero'));
 
 //Il programma stampa la somma di tutti i numeri inseriti.
 
-console.log(Number(numb1) + Number(numb2) + Number(numb3) + Number(numb4) + Number(numb5) + Number(numb6) + Number(numb7) + Number(numb8) + Number(numb9) + Number(numb10))
+console.log(Number(numb1) + Number(numb2) + Number(numb3) + Number(numb4) + Number(numb5) + Number(numb6) + Number(numb7) + Number(numb8) + Number(numb9) + Number(numb10)) */
+
+/* const body = document.querySelector('body')
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    const userNumber = Number(prompt('Inserisci un numero'));
+    sum += userNumber;
+}
+console.log(sum);
+body.innerHTML = sum; */
+
+const body = document.querySelector('body')
+let sum = 0;
+let i = 0
+while (i < 10) {
+    const userNumber = Number(prompt('Inserisci un numero:'));
+    sum += userNumber;
+    i++;
+}
+
+body.innerHTML = sum;
+
